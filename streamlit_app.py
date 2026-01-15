7f38c80106b33114a9c0962a6bf4fc2cf1b5c286
FatimahSaeed <55546717+FatimahSaeed@users.noreply.github.com>
2026-01-15 07:20:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import numpy as np
+import pickle
 
-st.title('🎈 App Name')
+model = pickle.load(open('insuranceML.sav', 'rb'))
 
-st.write('Hello world!')
+st.title('US Health Insurance Prediction')
+age = st.slider('Age', 1, 100)
+gender = st.number_input('Enter your gender: Male 0, Fmale 1')
+bmi = st.number_input('Enter the weight')
+children = st.slider('Childrens', 0, 20)
+smoker = st.number_input('Are you smoker? no: 0, yes: 1')
+options = [1, 2, 3, 4]
+region = st.selectbox('Choose your region: southeast 1, southwest 2, northwest 3, northeast 4', options)
+
+input_data = [age, gender, bmi, children, smoker, region]
+
+result = ''
+if st.button('Result'):
+  input_data = np.asarray(input_data).reshape(1,-1)
+  prediction = model.predict(input_data)
+  result = prediction[0]
+  
+
+st.success(result)
